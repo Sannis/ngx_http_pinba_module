ca487d0da66540c6c6b4a3de9e54b87ca79956ef
Oleg Efimov <o.efimov@corp.badoo.com>
2019-04-25 18:44:48 +0300

    fix typos

--- ngx_http_pinba_module.c
+++ ngx_http_pinba_module.c
@@ -202,8 +202,8 @@ static ngx_http_module_t  ngx_http_pinba_module_ctx = { /* {{{ */
     NULL,                                  /* create server configuration */
     NULL,                                  /* merge server configuration */
 
-    ngx_http_pinba_create_loc_conf,        /* create location configration */
-    ngx_http_pinba_merge_loc_conf          /* merge location configration */
+    ngx_http_pinba_create_loc_conf,        /* create location configuration */
+    ngx_http_pinba_merge_loc_conf          /* merge location configuration */
 };
 /* }}} */
 
